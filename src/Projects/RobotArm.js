e8b77ce8fefe79a2da333f61568321a95fd02b1f
Srijan Duggal <srijanduggal17@gmail.com>
2021-01-07 09:16:41 -0600

    robot arm page

--- src/Projects/RobotArm.js
+++ src/Projects/RobotArm.js
@@ -16,7 +16,7 @@ export default function RobotArm(props) {
 
 	const thirdPicStyle = {
 		height: '80vh',
-		width: '100%'
+		width: '80%'
 	};
 
 	const fourthPicStyle = {
@@ -59,47 +59,123 @@ export default function RobotArm(props) {
 				<FullRowText>
 				In 2017, the goal was to move various stacks of pennies into a central area to score points in a target-like fashion, where there were rings corresponding to different point values. This year, we build this project really nicely from our point of view. We went with a crane-like design, with one motor rotating the robot, another motor moving a carriage in the radial direction, and another motor moving the end effector vertically.
 				</FullRowText>
-
 				<div className="row">
-					<div className="col-sm-4 d-flex align-items-center" >
-						<img className="pt-4 mx-auto pb-4 d-block" src='project-assets/Electric Vehicle/EV 2016 breadboard.jpg' style={firstPicStyle}/>
+					<div className="col-sm-3 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Stepper Attachment.JPG' style={firstPicStyle}/>
 					</div>
-					<div className="col-sm-4 d-flex align-items-center" >
-						<img className="pt-4 mx-auto pb-4 d-block" src='project-assets/Electric Vehicle/EV 2016 protoboard.jpg' style={firstPicStyle}/>
+					<div className="col-sm-3 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Stepper Attachment 2.JPG' style={firstPicStyle}/>
 					</div>
 					<div className="col d-flex align-items-center" >
-						<img className="pt-4 mx-auto pb-4 d-block" src='project-assets/Electric Vehicle/EV 2016.jpg' style={firstPicStyle}/>
+						<iframe className="pt-4 d-block mx-auto"
+								src='https://www.youtube.com/embed/_1AaNBvEM7Q'
+								style={fourthPicStyle}/>
 					</div>
 				</div>
 				<FullRowText>
-				This worked but it was not very fast. Our next plan was to create the car ourselves. We used Fusion 360 to design the chassis and laser cut it out of some thin plywood. Then we used Knex (a children’s construction toy) to make the frame, relying on a DC motor to turn a Knex gear. The circuit remained the same as before. This was my first time actually using a DC motor and setting up an Arduino to control it which was really cool!
+				We started by screwing a hefty stepper motor with a threaded rod onto a wooden base. Then we used the corresponding nut for the linear actuator to mount a wooden stand. This stand was made by drilling a hole for the threaded rod into some pine, and scoring it to attach a piece of lauan wood with bolt holes for the nut. 
+				</FullRowText>
+				<div className="row">
+					<div className="col-sm-4 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/End Effector T.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/End Effector T with Motor.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col-sm-3 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/End Effector T Wiring.JPG' style={firstPicStyle}/>
+					</div>
+				</div>
+				<FullRowText>
+				Then we worked on the end effector a bit. We wanted to use a servo motor at the end, so we cut a slot for it into a PVC tee that we could pass the wires through.
+				</FullRowText>
+				<div className="row ml-auto">
+					<div className="col-sm-4 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Vertical Axis Attachment v1.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Vertical Axis.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col-sm-4 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Vertical Axis Spool Motor.JPG' style={firstPicStyle}/>
+					</div>
+				</div>
+				<FullRowText>
+				Next we worked on our vertical axis. We thought to have a pulley-type system bring this PVC tube up and down, as it could slide on another smaller PVC tube to maintain vertical alignment. Once we validated that our general idea worked, we started designing and building an actuation method. We had a stepper motor pull the belt and had a continuous rotation servo spool up the excess.
 				</FullRowText>
 				<div className="row">
 					<div className="col-sm-3 d-flex align-items-center" >
-						<img className="pt-4 mx-auto d-block" src='project-assets/Electric Vehicle/EV 2016 v2 Underside.JPG' style={secondPicStyle}/>
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Vertical Axis Spool Attachment.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col-sm-3 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Vertical Axis Spool and Stepper.JPG' style={firstPicStyle}/>
 					</div>
 					<div className="col d-flex align-items-center" >
-						<img className="pt-4 mx-auto d-block" src='project-assets/Electric Vehicle/EV 2016 v2 Overall.JPG' style={secondPicStyle}/>
+						<iframe className="pt-4 d-block mx-auto"
+								src='https://www.youtube.com/embed/nPHlaa02rLk'
+								style={fourthPicStyle}/>
 					</div>
-					<div className="col-sm-6 d-flex align-items-center" >
-						<iframe className="pt-4 d-block"
-								src='https://www.youtube.com/embed/UHVh_QjFfiA'
+				</div>
+				<FullRowText>
+				Once that was verified, we started working on the carriage and rails for motion along the radial axis. We decided to make the rails out of PVC tubing, as it was pretty light and seemed sturdy enough. We laser cut and built some mounts for the tubing out of wood. Then we attached a pulley at the other end of the PVC. The idea was to have a stepper motor drive a belt around the pulley, and have part of the belt connected to the vertical axis carriage.
+				</FullRowText>
+				<div className="row">
+					<div className="col-sm-4 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Radial Axis PVC Holder.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Radial Axis End Pulley.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col-sm-4 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Radial Axis Shu Working.JPG' style={firstPicStyle}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-5 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Radial Axis Idea.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col d-flex align-items-center" >
+						<iframe className="pt-4 d-block mx-auto"
+								src='https://www.youtube.com/embed/7FDt0CleR14'
+								style={fourthPicStyle}/>
+					</div>
+				</div>
+				<FullRowText>
+				Next we added some supports to our vertical axis carriage to ensure it would slide in a straight line and not just flop around.
+				</FullRowText>
+				<div className="row">
+					<div className="col-sm-3 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Vertical Axis Holder with Supports.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col-sm-3 d-flex align-items-center" >
+						<img className="pt-4 mx-auto d-block" src='project-assets/Robot Arm/Radial Axis Overview.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col d-flex align-items-center" >
+						<iframe className="pt-4 d-block mx-auto"
+								src='https://www.youtube.com/embed/a4SEc4N7se0'
+								style={fourthPicStyle}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-6" >
+						<img className="pt-4 mx-auto pb-4 d-block" src='project-assets/Robot Arm/Electronics Breadboarding.JPG' style={firstPicStyle}/>
+					</div>
+					<div className="col text-start fs-5 d-flex align-items-center pt-4">
+						<p>
+						Finally we assembled everything together and tested it! We tried a variety of 3D printed “tools” at the end effector as shown in the videos.
+						</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col">
+						<iframe className="pt-4 d-block mx-auto"
+								src='https://www.youtube.com/embed/0olJFrZW1fQ'
 								style={thirdPicStyle}/>
 					</div>
 				</div>
 				<FullRowText>
-				The following year, we went with a very similar approach, but we wanted to improve the alignment, so we 3D printed a chassis to keep the front and rear axles properly aligned. Another distinct change to improve the speed was to put a large gear on the motor shaft and attach a small gear onto the axle, such that the gear system would amplify the velocity. This ended up working really well!
+				All in all, these were some of the shaping moments early on in my career as an engineer. I learned a lot and I loved working with wood and electronics to help make our ideas come true. Without Science Olympiad, I don’t know if I ever would have decided to pursue engineering.
 				</FullRowText>
-				<div className="row mb-4">
-					<div className="col-md-6" >
-						<img className="pt-4 mx-auto d-block" src='project-assets/Electric Vehicle/EV Chassis.JPG' style={secondPicStyle}/>
-					</div>
-					<div className="col mx-auto d-flex align-items-center" >
-						<iframe className="pt-4 d-block"
-								src='https://www.youtube.com/embed/qkPnuEpzoS4'
-								style={thirdPicStyle}/>
-					</div>
-				</div>
 			</div>
 		</div>
 	)
