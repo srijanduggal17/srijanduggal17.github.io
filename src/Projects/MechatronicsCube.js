f1a1fcdaa182e70f84b006f182f5deaaf53dbd33
Srijan Duggal <srijanduggal17@gmail.com>
2021-07-29 19:06:59 -0400

    adding code link to mechatronics cube page

--- src/Projects/MechatronicsCube.js
+++ src/Projects/MechatronicsCube.js
@@ -234,6 +234,11 @@ export default function MechatronicsCube(props) {
 					</div>
 				</div>
 				<FullRowImage src='project-assets/Mechatronics Cube/Oscillation Trial Example.png'/>
+				<p className="fs-5 text-center pt-4">
+					The code can be found <a
+				href='https://github.com/srijanduggal17/ME4405-Reaction-Wheel-Balancing-Cube'>
+					here</a> on GitHub. Please contact me if you are interested in the CAD files.
+				</p>
 			</div>
 			<p></p>
 		</div>
